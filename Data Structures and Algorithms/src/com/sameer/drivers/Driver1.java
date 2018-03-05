6ce0e3cf1b1253a426e39ff759ff9ade6ba9404a
ssahx <ssahx@ssahx-MOBL1.gar.corp.intel.com>
2018-03-05 12:50:23 +0530

    Added LinkedList.java

--- Data Structures and Algorithms/src/com/sameer/drivers/Driver1.java
+++ Data Structures and Algorithms/src/com/sameer/drivers/Driver1.java
@@ -1,8 +1,31 @@
 package com.sameer.drivers;
 
+import com.sameer.dataStructures.LinkedList;
+
 public class Driver1 {
 
 	public static void main (String args[]) {
-		System.out.println("Hello, World !"); 
+		
+		LinkedList list = new LinkedList();
+		
+		list.add(20);
+		
+		list.add(30);
+		
+		list.add(40);
+		
+		list.add(50);
+		
+		list.display();
+		
+		System.out.println("Size: "+list.size());
+		
+		list.remove(1);
+		
+		System.out.println("Size: "+list.size());
+		
+		list.display();
+		
+		System.out.println(list);
 	}
 }
