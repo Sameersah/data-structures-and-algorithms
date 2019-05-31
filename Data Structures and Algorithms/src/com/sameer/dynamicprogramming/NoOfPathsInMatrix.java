94bb7124d0d4496bef61ee2e5481c7ffdd296a27
sameer sah <sameer_sah@yahoo.co.in>
2019-05-31 14:02:35 +0530

    Added backtracking problems

--- Data Structures and Algorithms/src/com/sameer/dynamicprogramming/NoOfPathsInMatrix.java
+++ Data Structures and Algorithms/src/com/sameer/dynamicprogramming/NoOfPathsInMatrix.java
@@ -1,23 +1,36 @@
 package com.sameer.dynamicprogramming;
 
+import java.util.Arrays;
+
+//topDown
 public class NoOfPathsInMatrix {
 
+    static Integer solMatrix[][];
     public static void main(String args[]){
 
         int rows = 2;
         int cols = 3;
 
-
+        solMatrix= new Integer[2][3];
+        for(int i=0;i<solMatrix.length;i++)
+        Arrays.fill(solMatrix[i],-1);
         System.out.println("No of ways to reach bottom right grid from top left grid is "+ getNoOfPaths(rows-1,cols-1));
     }
 
     private static int getNoOfPaths(int m, int n){
         if(m==0 || n==0){
-            return 1;
+            solMatrix[m][n] = 1;
+            return  solMatrix[m][n];
+        }else if(solMatrix[m][n] != -1){
+            return  solMatrix[m][n];
         }
-        else{
-            return getNoOfPaths(m-1,n)+getNoOfPaths(m,n-1);
+        else {
+          //  return getNoOfPaths(m-1,n)+getNoOfPaths(m,n-1);
+            solMatrix[m][n] = getNoOfPaths( m-1, n)+getNoOfPaths( m,n-1);
+            return  solMatrix[m][n];
         }
+
+
     }
 }
 
